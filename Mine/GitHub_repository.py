8794238b6910848e90fec88995b204b518a58ed0
Willem Adriaan Voges <wavoges@gmail.com>
2018-11-25 17:26:24 +0200

    Delete GitHub_repository.py

--- Mine/GitHub_repository.py
+++
@@ -1,7 +0,0 @@
-echo "# Student" >> README.md
-git init
-git add README.md
-git commit - m "first commit"
-git remote
-add origin https: // github.com / M45t3rJ4ck / Student.git
-git push - u origin master
